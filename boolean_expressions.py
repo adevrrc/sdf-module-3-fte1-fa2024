7b9df908e5515b985b07a20eb412975540fdf0b7
Damien Altenburg <daltenburg@rrc.ca>
2024-09-23 09:00:35 -0500

    Add script to demonstrate boolean expressions.

--- boolean_expressions.py
+++ boolean_expressions.py
@@ -4,3 +4,24 @@ Author:
 Date:
 Usage: python boolean_expressions.py
 """
+print(True)
+print(False)
+
+is_deron_here: bool = True
+print(is_deron_here)
+
+name: str = "Damien"
+print(name.isalpha())
+print(name.isdigit())
+
+age: int = 25
+
+print(age == 25)
+print(age == 35)
+print(age != 35)
+print(age < 14)
+print(age <= 25)
+print(age > 14)
+print(age >= 100)
+
+are_you_old: bool = age >= 100
