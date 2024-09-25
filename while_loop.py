f55d4050b1fcf87d902b49c9611c2f7a53a2dd13
Damien Altenburg <daltenburg@rrc.ca>
2024-09-25 16:22:23 -0500

    Update to include example of a while loop.

--- while_loop.py
+++ while_loop.py
@@ -4,3 +4,14 @@ Author:
 Date:
 Usage: python while_loop.py
 """
+new_password = "new_password"
+confirm_password = "confirm_password"
+
+while new_password != confirm_password:
+    new_password = input("Enter the new password: ")
+
+    confirm_password = input("Confirm password: ")
+
+    print("Confirmation not successful. Try again." if \
+          new_password != confirm_password else \
+          "Password Updated!")
