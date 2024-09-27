1eb7c6cc6298c6b83a31dfbee712e2c7b09cc387
Damien Altenburg <daltenburg@rrc.ca>
2024-09-27 14:56:48 -0500

    Update that includes examples of opening, reading, and closing a file.

--- file_input.py
+++ file_input.py
@@ -1,6 +1,24 @@
 """
 Description: A script to demonstrate file input.
-Author:
-Date:
+Author: Damien Altenburg
+Date: 2024-9-27
 Usage: python file_input.py
 """
+file_path: str = "./data/data.txt"
+
+people_to_hack = open(file_path, 'r')
+
+data: str = people_to_hack.read()
+
+people_to_hack.close()
+
+#print(data)
+
+people_to_hack = open(file_path, 'r')
+
+#print(people_to_hack.readline())
+
+for line in people_to_hack:
+    print(line.rstrip())
+
+people_to_hack.close()
