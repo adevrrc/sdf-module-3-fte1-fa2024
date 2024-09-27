1d134cfcce1a587090f79b1a38427a5d50b2ea2f
Damien Altenburg <daltenburg@rrc.ca>
2024-09-27 14:56:10 -0500

    Update to include examples of importing built-in Python module math and random.

--- python_modules.py
+++ python_modules.py
@@ -1,8 +1,21 @@
 """
 Description: A script to demonstrate python modules; math and random.
-Author:
-Date:
-Usage: python_modules.py
+Author: Damien Altenburg
+Date: 2024-9-27
+Usage: python python_modules.py
 """
+from math import isqrt, pow
+from random import randint
 
 teams = ["jets", "flames", "oilers", "canucks", "senators", "canadians"]
+
+print(isqrt(64))
+print(pow(2, 3))
+
+player_dice = []
+
+for number in range(5):
+    die_roll = randint(1, 6)
+    player_dice.append(die_roll)
+
+print(player_dice)
