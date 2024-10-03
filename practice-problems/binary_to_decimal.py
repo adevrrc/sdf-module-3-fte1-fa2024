ad18b3c7f3a58f3927a7d385314dbc055a88185b
Damien Altenburg <daltenburg@rrc.ca>
2024-10-03 07:14:34 -0500

    Add a directory for practice problems and added a new script for problem 37.

---
+++ practice-problems/binary_to_decimal.py
@@ -0,0 +1,23 @@
+"""
+Description: Write a program that takes a word representing a binary number 
+(0s and 1s) as an input and converts it to its decimal representation; for 
+instance, if the user inputs 101, then the output will be 5; you can assume 
+that the input is guaranteed to contain only 0s and 1s.
+
+Author: Damien Altenburg
+Date: 2024-10-2
+Usage: python binary_to_decimal.py
+"""
+binary_number = input("Enter binary number: ")
+
+decimal_number = 0
+exponent = len(binary_number)
+
+for digit in binary_number:
+    binary_digit = int(digit)
+
+    exponent -= 1
+
+    decimal_number += binary_digit * 2 ** exponent
+
+print(f"The {binary_number} in decimal is {decimal_number}.")
